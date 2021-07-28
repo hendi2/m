251a1fbfb33cb11f2fbb328ebe06ceedb1883c1c
hendi2 <76987268+hendi2@users.noreply.github.com>
2021-07-28 08:53:33 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <img src="https://avatars.githubusercontent.com/u/76987268?v=4" alt="alternatetext"></img>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
